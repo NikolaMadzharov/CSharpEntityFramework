c5b99d936f2078af33ce417906e86bd326d6ef46
nikola madzharov <nmadzharov1912@gmail.com>
2022-07-27 23:16:24 +0300

    100/100

---
+++ Entity Framework Introduction/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using SoftUni.Data;
+using SoftUni.Models;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            SoftUniContext context = new SoftUniContext();
+
+            //Console.WriteLine(GetEmployeesFullInformation(context));
+            //Console.WriteLine(GetEmployeesWithSalaryOver50000(context));
+            //Console.WriteLine(GetEmployeesFromResearchAndDevelopment(context));
+            Console.WriteLine(AddNewAddressToEmployee(context));
+
+        }
+
+        public  static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            var employeeInformation = context.Employees
+                .Select(x => new
+                {
+                    x.EmployeeId,
+                    x.FirstName,
+                    x.MiddleName,
+                    x.LastName,
+                    x.JobTitle,
+                    x.Salary
+                })
+                .OrderBy(x=>x.EmployeeId)
+                .ToArray();
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (var employee in employeeInformation)
+            {
+                
+                output.AppendLine($"{employee.FirstName} {employee.LastName} {employee.MiddleName} {employee.JobTitle} {employee.Salary:f2}");
+            }
+
+            return output.ToString().Trim();
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employeeSalary = context.Employees
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.Salary
+                })
+                .Where(x => x.Salary > 50000)
+                .OrderBy(x => x.FirstName)
+                .ToArray();
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (var employee in employeeSalary)
+            {
+                output.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employeeDetails = context.Employees
+                .Select(x => new
+                {
+                    x.FirstName,
+                    x.LastName,
+                    DepartmentName = x.Department.Name,
+                    x.Salary,
+                })
+                .Where(x => x.DepartmentName == "Research and Development")
+                .OrderBy(x => x.Salary)
+                .ThenByDescending(x => x.FirstName)
+                .ToArray();
+
+            StringBuilder output = new StringBuilder();
+
+            foreach (var employee in employeeDetails)
+            {
+                output.AppendLine($"{employee.FirstName} {employee.LastName} from {employee.DepartmentName} - ${employee.Salary:f2}");
+            }
+
+            return output.ToString().TrimEnd();
+        }
+
+        public static string AddNewAddressToEmployee(SoftUniContext context)
+        {
+            Address address = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownId = 4
+            };
+
+            Employee nakovEmployee = context.Employees.First(x => x.LastName == "Nakov");
+
+            context.Addresses.Add(address);
+
+            nakovEmployee.Address = address;
+
+            context.SaveChanges();
+
+
+            var employee = context.Employees
+                .Select(x => new
+                {
+                    x.AddressId,
+                    adressText = x.Address.AddressText
+                })
+                .OrderByDescending(x => x.AddressId)
+                .Take(10)
+                .ToArray();
+            StringBuilder output = new StringBuilder();
+
+            foreach (var em in employee)
+            {
+                output.AppendLine(em.adressText);
+            }
+
+            return output.ToString().TrimEnd();
+        }
+    }
+}
