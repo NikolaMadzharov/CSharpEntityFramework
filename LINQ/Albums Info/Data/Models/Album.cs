0ca6e90255eb1ad5f2032adf09564ab3b19f348a
nikola madzharov <nmadzharov1912@gmail.com>
2022-08-02 15:33:41 +0300

    100/100

---
+++ LINQ/Albums Info/Data/Models/Album.cs
@@ -0,0 +1,39 @@
+﻿namespace MusicHub.Data.Models
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+
+    using Castle.DynamicProxy.Generators.Emitters;
+
+    [SuppressMessage("ReSharper", "StyleCop.SA1600")]
+    public class Album
+    {
+        public Album()
+        {
+            this.Songs = new HashSet<Song>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public decimal Price => this.Songs.Sum(x => x.Price);
+
+        [ForeignKey(nameof(Producer))]
+        public int? ProducerId { get; set; }
+        public virtual Producer Producer { get; set; }
+
+        public ICollection<Song> Songs { get; set; }
+
+    }
+}
---
+++ LINQ/Albums Info/Data/MusicHubDbContext.cs
@@ -0,0 +1,42 @@
+﻿namespace MusicHub.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using MusicHub.Data.Models;
+
+    public class MusicHubDbContext : DbContext
+    {
+        public MusicHubDbContext()
+        {
+        }
+
+        public MusicHubDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Album> Album { get; set; }
+
+        public virtual DbSet<Performer> Performers { get; set; }
+
+        public virtual DbSet<Song> Songs { get; set; }
+
+        public virtual DbSet<SongPerformer> SongPerformers { get; set; }
+
+        public virtual  DbSet<Writer> Writers { get; set; }
+
+        public virtual DbSet<Producer> Producers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<SongPerformer>().HasKey(x => new { x.PerformerId, x.SongId });
+        }
+    }
+}
