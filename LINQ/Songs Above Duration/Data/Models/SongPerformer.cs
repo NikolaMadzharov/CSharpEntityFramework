f77b25f9b1d2c826cd230b5680603c13212fef98
nikola madzharov <nmadzharov1912@gmail.com>
2022-08-02 15:34:33 +0300

    100/100

---
+++ LINQ/Songs Above Duration/Data/Models/SongPerformer.cs
@@ -0,0 +1,18 @@
+﻿namespace MusicHub.Data.Models
+{
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class SongPerformer
+    {
+        
+        public int SongId { get; set; }
+
+        public virtual Song Song { get; set; }
+
+        public int PerformerId { get; set; }
+
+        public virtual Performer Performer { get; set; }
+
+        
+    }
+}
